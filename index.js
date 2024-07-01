1339e4329a6659995d1ad24761e81df87f77f943
Parth-243 <parthshrivastva24@gmail.com>
2024-07-02 00:03:25 +0530

    create basic express routings

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const express = require('express');
 // Initialize express instance
 const app = express();
 
+app.get('/', (request, response) =>
+    response.send('Welcome to fast food backend!')
+);
+
+// handle unknown routes
+app.all('*', (request, response) =>
+    response.status(404).send('Resource not found.')
+);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.info(`Server up and running on port ${port} !`));
