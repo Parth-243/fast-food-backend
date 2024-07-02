c95270ebe80f5440b9e20805d546c358c40b7a3c
Parth-243 <parthshrivastva24@gmail.com>
2024-07-02 23:39:15 +0530

    create a main routing file fix the mongoose import issue fix the config index env const issue

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const { env } = process;
+
 const config = {
   PORT: env.PORT || 4000,
   MONGO_URI: `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DATABASE}?authMechanism=DEFAULT&authSource=admin`,
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const config = require('./config');
 
 // Initialize mongodb connection
@@ -12,13 +14,18 @@ mongoose.set('debug', process.env.NODE_ENV === 'development');
 // Initialize express instance
 const app = express();
 
-app.get('/', (request, response) =>
-    response.send('Welcome to fast food backend!')
-);
+// parse body params and attache them to req.body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// registering app routes
+app.use(routes);
 
 // handle unknown routes
 app.all('*', (request, response) =>
-    response.status(404).send('Resource not found.')
+  response.status(404).send('Resource not found.')
 );
 
-app.listen(config.PORT, () => console.info(`Server up and running on port ${config.PORT} !`));
+app.listen(config.PORT, () =>
+  console.info(`Server up and running on port ${config.PORT} !`)
+);
