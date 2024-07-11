f26151020d9c0ff7f4310ca1c63f4ab8e8015d35
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 03:02:01 +0530

    get the jwt expiry time from env in config

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const config = {
   PORT: env.PORT || 4000,
   MONGO_URI: `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DATABASE}?authMechanism=DEFAULT&authSource=admin`,
   JWT_SECRET: env.JWT_SECRET,
-  JWT_EXPIRY_TIME: '1h',
+  JWT_EXPIRY_TIME: env.JWT_EXPIRY_TIME || '1h',
   MIN_PASSWORD_LENGTH: parseInt(env.MIN_PASSWORD_LENGTH || 8, 10),
   USER_ROLES: { USER: 'user', B_USER: 'businessUser', ADMIN: 'admin' },
 };
