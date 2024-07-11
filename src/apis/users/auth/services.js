7c8baad6927306eccaae897d98bc8baf74a28788
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:50:30 +0530

    create a registration service

---
+++ src/apis/users/auth/services.js
@@ -0,0 +1,49 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY_TIME } = require('../../../../config');
+
+exports.findUserByUsernameOrEmail = async (username, email, role) => {
+  return await User.find({
+    $or: [
+      { email: email.toLowerCase(), role },
+      { username: username.toLowerCase(), role },
+    ],
+  });
+};
+
+exports.register = async ({ username, email, password, role }) => {
+  const existingUsers = await this.findUserByUsernameOrEmail(
+    username,
+    email,
+    role
+  );
+
+  let errors = {};
+  for (const user of existingUsers) {
+    if (user.username === username.toLowerCase()) {
+      errors.username = 'Username already exists';
+    }
+    if (user.email === email.toLowerCase()) {
+      errors.email = 'Email already exists';
+    }
+  }
+
+  if (Object.keys(errors).length > 0) {
+    throw new Error(JSON.stringify(errors));
+  }
+
+  const user = new User({
+    username: username.toLowerCase(),
+    email: email.toLowerCase(),
+    password, // Pass the plain password, it will be hashed in the model
+    role,
+  });
+
+  await user.save();
+
+  const token = jwt.sign({ id: user._id, role }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRY_TIME,
+  });
+
+  return { user, token };
+};
