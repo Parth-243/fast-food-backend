69e3ac2ed079ab4889b1d12f5f25c883546f71ca
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:44:35 +0530

    update the user login api to validate data before passing data to user controller

--- src/apis/users/auth/routes.js
+++ src/apis/users/auth/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const controller = require('./controller');
-const { validateRegister } = require('./validator');
+const { validateLogin, validateRegister } = require('./validator');
 
-router.post('/login', controller.userLogin);
+router.post('/login', validateLogin, controller.userLogin);
 router.post('/register', validateRegister, controller.userRegister);
 
 module.exports = router;
