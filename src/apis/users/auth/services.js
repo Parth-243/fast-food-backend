bbf81b5f552cda3acd473d508fa8c7f9983ef10c
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:54:03 +0530

    create a login service for user login api

--- src/apis/users/auth/services.js
+++ src/apis/users/auth/services.js
@@ -1,6 +1,16 @@
 const User = require('../../../models/user');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRY_TIME } = require('../../../../config');
+
+exports.findUserByIdentifier = async (identifier, role) => {
+  return await User.findOne({
+    $or: [
+      { email: identifier.toLowerCase(), role },
+      { username: identifier.toLowerCase(), role },
+    ],
+  });
+};
 
 exports.findUserByUsernameOrEmail = async (username, email, role) => {
   return await User.find({
@@ -9,6 +19,25 @@ exports.findUserByUsernameOrEmail = async (username, email, role) => {
       { username: username.toLowerCase(), role },
     ],
   });
+};
+
+exports.login = async ({ identifier, password, role }) => {
+  const user = await this.findUserByIdentifier(identifier, role);
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Invalid credentials');
+  }
+
+  const token = jwt.sign({ id: user._id, role }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRY_TIME,
+  });
+
+  return { user, token };
 };
 
 exports.register = async ({ username, email, password, role }) => {
@@ -46,4 +75,10 @@ exports.register = async ({ username, email, password, role }) => {
   });
 
   return { user, token };
+};
+
+exports.logout = async (res) => {
+  res.cookie('x-access-token', '', { expires: new Date(0) });
+  res.clearCookie('x-access-token');
+  return { message: 'Logout success' };
 };
