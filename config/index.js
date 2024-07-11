1a69d0163d520adceb500b3c49ee0e45b2cadf9f
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:20:54 +0530

    add required constants in the config file

--- config/index.js
+++ config/index.js
@@ -5,6 +5,9 @@ const { env } = process;
 const config = {
   PORT: env.PORT || 4000,
   MONGO_URI: `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DATABASE}?authMechanism=DEFAULT&authSource=admin`,
+  JWT_SECRET: env.JWT_SECRET,
+  JWT_EXPIRY_TIME: '1h',
+  MIN_PASSWORD_LENGTH: parseInt(env.MIN_PASSWORD_LENGTH || 8, 10),
   USER_ROLES: { USER: 'user', B_USER: 'businessUser', ADMIN: 'admin' },
 };
 
