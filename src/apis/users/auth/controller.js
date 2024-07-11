41306e77f9db05fe1e11050d67c1a0a39c4425c5
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:52:54 +0530

    update the user registration api to save user information

--- src/apis/users/auth/controller.js
+++ src/apis/users/auth/controller.js
@@ -1,3 +1,6 @@
+const { register } = require('./services');
+const { USER_ROLES } = require('../../../../config');
+
 // User login
 async function userLogin(req, res) {
   try {
@@ -12,9 +15,23 @@ async function userLogin(req, res) {
 async function userRegister(req, res) {
   try {
     const { username, email, password } = req.body;
-    res.status(200).json({ body: req.body });
+    const user = await register({
+      username,
+      email,
+      password,
+      role: USER_ROLES.USER,
+    });
+    res.status(201).json(user);
   } catch (error) {
+    let errors = {};
+    try {
+      errors = JSON.parse(error.message);
+    } catch (e) {
       errors.message = error.message;
+    }
+    res.status(400).json(errors);
+  }
+}
 
   }
 }
