99b51f95a3a7d13d6a679ed7e94a3cd4f63b3098
Parth-243 <parthshrivastva24@gmail.com>
2024-07-12 02:43:46 +0530

    create a validation for user login api

--- src/apis/users/auth/validator.js
+++ src/apis/users/auth/validator.js
@@ -1,6 +1,43 @@
 const { check, validationResult, body } = require('express-validator');
 const { MIN_PASSWORD_LENGTH } = require('../../../../config');
 
+exports.validateLogin = [
+  body().custom((body) => {
+    if (!body.email && !body.username) {
+      throw new Error('Either email or username must be provided');
+    }
+    return true;
+  }),
+  check('email')
+    .optional()
+    .isEmail()
+    .withMessage('Please provide a valid email'),
+  check('username')
+    .optional()
+    .notEmpty()
+    .withMessage('Username is required if email is not provided'),
+  check('password')
+    .isLength({ min: MIN_PASSWORD_LENGTH })
+    .withMessage(
+      `Password must be at least ${MIN_PASSWORD_LENGTH} characters long`
+    )
+    .matches(/[A-Z]/)
+    .withMessage('Password must contain at least one uppercase letter')
+    .matches(/\d/)
+    .withMessage('Password must contain at least one digit')
+    .matches(/[@$!%*?&]/)
+    .withMessage(
+      'Password must contain at least one special symbol (@, $, !, %, *, ?, &)'
+    ),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
 exports.validateRegister = [
   check('username').notEmpty().withMessage('Username is required'),
   check('email').isEmail().withMessage('Please provide a valid email'),
