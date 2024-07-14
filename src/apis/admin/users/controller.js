77bf8c5bd0cc7a35af91cbf42a36b4b527d507f6
Parth-243 <parthshrivastva24@gmail.com>
2024-07-15 04:37:22 +0530

    update the admin get all customer api to include the user profile info in response

--- src/apis/admin/users/controller.js
+++ src/apis/admin/users/controller.js
@@ -3,7 +3,45 @@ const User = require('../../../models/user');
 
 async function getAllCustomers(req, res) {
   try {
-    const users = await User.find({ role: USER_ROLES.USER });
+    const users = await User.aggregate([
+      { $match: { role: USER_ROLES.USER } },
+      {
+        $lookup: {
+          from: 'userprofiles',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'profile',
+        },
+      },
+      {
+        $unwind: {
+          path: '$profile',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          firstName: { $ifNull: ['$profile.firstName', null] },
+          lastName: { $ifNull: ['$profile.lastName', null] },
+          dob: { $ifNull: ['$profile.dob', null] },
+          gender: { $ifNull: ['$profile.gender', null] },
+          mobile: { $ifNull: ['$profile.mobile', null] },
+          address: { $ifNull: ['$profile.address', null] },
+          state: { $ifNull: ['$profile.state', null] },
+          city: { $ifNull: ['$profile.city', null] },
+          postalCode: { $ifNull: ['$profile.postalCode', null] },
+          picture: { $ifNull: ['$profile.picture', null] },
+        },
+      },
+      {
+        $project: {
+          password: 0,
+          __v: 0,
+          profile: 0,
+        },
+      },
+    ]);
+
     res.status(200).json(users);
   } catch (error) {
     res.status(400).json({ message: error.message });
