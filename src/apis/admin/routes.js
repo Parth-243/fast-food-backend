d3fa72510b0b455212e5ab57670021da0dcbf5d1
Parth-243 <parthshrivastva24@gmail.com>
2024-07-15 01:09:41 +0530

    register food category routes in admin routing file

--- src/apis/admin/routes.js
+++ src/apis/admin/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRoutes = require('./auth/routes');
+const foodCategoryRoutes = require('./foodCategory/routes');
 
 router.use('/auth', authRoutes);
+router.use('/food-categories', foodCategoryRoutes);
 
 module.exports = router;
