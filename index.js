8353609904d8bdb008f3538e63c6b4a01830cd01
Parth-243 <parthshrivastva24@gmail.com>
2024-07-14 21:10:31 +0530

    allow the specific urls by updating the cors config

--- index.js
+++ index.js
@@ -18,8 +18,19 @@ const app = express();
 
 app.use(cookieParser());
 
-// Enable CORS for all origins
-app.use(cors());
+// Enable CORS for specific origins with credentials
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (config.ALLOWED_ORIGINS.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // parse body params and attach them to req.body
 app.use(express.urlencoded({ extended: false }));
