12d492068e1019b08dcd127a3908e94bd1041e1b
Parth-243 <parthshrivastva24@gmail.com>
2024-07-14 19:17:35 +0530

    create route config for profile picture upload and create route to upload profile picture

--- src/apis/users/profile/routes.js
+++ src/apis/users/profile/routes.js
@@ -5,11 +5,23 @@ const {
   validateUpdateUserProfile,
 } = require('./validator');
 const authenticateUser = require('../../middleware/authenticateUser');
+const createMulterConfig = require('../../middleware/multerConfig');
+const {
+  ALLOWED_IMAGE_TYPES,
+  MAX_IMAGE_FILE_SIZE_IN_MB,
+} = require('../../../../config');
+
+const uploadConfig = createMulterConfig(
+  MAX_IMAGE_FILE_SIZE_IN_MB * 1024 * 1024,
+  1,
+  ALLOWED_IMAGE_TYPES
+);
 
 router.use(authenticateUser);
 
 router.post('/', validateCreateUserProfile, controller.createUserProfile);
 router.get('/', controller.getUserProfile);
 router.put('/', validateUpdateUserProfile, controller.updateUserProfile);
+router.post('/picture', uploadConfig.single('file'), controller.uploadPicture);
 
 module.exports = router;
