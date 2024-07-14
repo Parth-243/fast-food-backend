08de4e11e62989e0a853b961736f7e9409004329
Parth-243 <parthshrivastva24@gmail.com>
2024-07-14 12:02:32 +0530

    register restaurant routes in api main routing file

--- src/apis/business/routes.js
+++ src/apis/business/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRoutes = require('./auth/routes');
+const restaurantRoutes = require('./restaurant/routes');
 
 router.use('/auth', authRoutes);
+router.use('/restaurant', restaurantRoutes);
 
 module.exports = router;
