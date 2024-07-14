67350e2eff615d9065078043dca77c89c01e25bb
Parth-243 <parthshrivastva24@gmail.com>
2024-07-14 12:03:05 +0530

    create a controller for restaurant

---
+++ src/apis/business/restaurant/controller.js
@@ -0,0 +1,98 @@
+const Restaurant = require('../../../models/restaurant');
+
+// Create a new restaurant
+exports.createRestaurant = async (req, res) => {
+  try {
+    const restaurant = new Restaurant(req.body);
+    restaurant.userId = req.user._id; // Assign the current user as the owner
+    await restaurant.save();
+    res.status(201).send(restaurant);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Get all restaurants for the logged-in user
+exports.getAllRestaurants = async (req, res) => {
+  try {
+    const userId = req.user._id; // Assuming res.user contains the logged-in user's information
+    const restaurants = await Restaurant.find({ userId });
+    res.status(200).send(restaurants);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Get a single restaurant by ID
+exports.getRestaurantById = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findOne({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+    if (!restaurant) {
+      return res.status(404).send();
+    }
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Update a restaurant by ID
+exports.updateRestaurantById = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [
+    'name',
+    'phone',
+    'opensAt',
+    'closesAt',
+    'address',
+    'city',
+    'state',
+    'pincode',
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const restaurant = await Restaurant.findOne({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
+    if (!restaurant) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (restaurant[update] = req.body[update]));
+    await restaurant.save();
+
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// Delete a restaurant by ID
+exports.deleteRestaurantById = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user._id,
+    });
+
+    if (!restaurant) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
