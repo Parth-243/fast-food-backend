914427a0c5a9a9404ddcf1513d60010e0e9f64a3
Parth-243 <parthshrivastva24@gmail.com>
2024-07-17 23:15:55 +0530

    create get and list food category apis for business user

--- src/apis/business/routes.js
+++ src/apis/business/routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const authRoutes = require('./auth/routes');
 const restaurantRoutes = require('./restaurant/routes');
+const foodCategoryRoutes = require('./foodCategory/routes');
 
 router.use('/auth', authRoutes);
 router.use('/restaurant', restaurantRoutes);
+router.use('/food-categories', foodCategoryRoutes);
 
 module.exports = router;
