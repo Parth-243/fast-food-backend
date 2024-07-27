227cbd100e7a7229037e864cf57c7b45651e659e
Parth-243 <parthshrivastva24@gmail.com>
2024-07-27 23:59:11 +0530

    create a validator for getFoodsByCategories business controller

--- src/apis/business/food/validator.js
+++ src/apis/business/food/validator.js
@@ -1,4 +1,4 @@
-const { check, validationResult } = require('express-validator');
+const { check, query, validationResult } = require('express-validator');
 
 exports.validateCreateFood = [
   check('restaurantId')
@@ -50,6 +50,30 @@ exports.validateUpdateFood = [
     .optional()
     .isFloat({ min: 0 })
     .withMessage('Price must be a positive number'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const extractedErrors = {};
+      errors.array().forEach((err) => {
+        extractedErrors[err.path] = err.msg;
+      });
+      return res.status(400).json(extractedErrors);
+    }
+    next();
+  },
+];
+
+exports.validateGetFoodsByCategories = [
+  query('restaurantId')
+    .notEmpty()
+    .withMessage('Restaurant ID is required')
+    .bail() // Stop running further validations if this validation fails
+    .isMongoId()
+    .withMessage('Restaurant ID must be a valid ID'),
+  query('foodCategoryId')
+    .optional()
+    .isMongoId()
+    .withMessage('Food Category ID must be a valid ID'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
